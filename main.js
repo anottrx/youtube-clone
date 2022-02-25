05822be02ec345fa53c03458b019a8191c9bf375
anottrx <anottrx@gmail.com>
2022-02-25 14:24:45 +0900

    fix : 비디오와 사진 추가 후 태그 수정

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const thumbsUpBtn = document.querySelector("#thumbsUp");
 const thumbsUpCnt = document.querySelector(".thumbsUpAndCnt");
 const subscribeBtn = document.querySelector("#subscribeBtn");
 let isClicked = true;
+let isSubscribed = true;
 
 thumbsUpBtn.addEventListener("click", () => {
   if (isClicked) {
@@ -17,5 +18,12 @@ thumbsUpBtn.addEventListener("click", () => {
 });
 
 subscribeBtn.addEventListener("click", () => {
+  if (isSubscribed) {
+    document.getElementById("subscribeBtn").innerHTML = "구독중";
+    isSubscribed = !isSubscribed;
+  } else {
+    document.getElementById("subscribeBtn").innerHTML = "Subscribe";
+    isSubscribed = !isSubscribed;
+  }
   subscribeBtn.classList.toggle("active");
 });
